fc71b9a51b0d3b38fe50ef658842c8f3b4cf9429
Agent BUB <46576969+AgentBUB@users.noreply.github.com>
2022-01-31 16:08:54 -0500

    EJ Rework

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 5000
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.status(200).render('index')
+})
+
+app.get('/commands', (req, res) => {
+  res.status(200).render('commands')
+})
+
+app.get('/changelogs', (req, res) => {
+  res.status(200).render('changelogs')
+})
+
+app.get('/info', (req, res) => {
+  res.status(200).render('info')
+})
+
+app.get('/policies', (req, res) => {
+  res.status(200).render('policies')
+})
+
+app.get('/thanks', (req, res) => {
+  res.status(200).render('thanks')
+})
+
+app.get('/invite', (req, res) => {
+  res
+    .status(200)
+    .redirect(
+      'https://discord.com/oauth2/authorize?client_id=740023863479631943&permissions=1513938742527&scope=bot'
+    )
+})
+
+app.get('/support', (req, res) => {
+  res.status(200).redirect('https://discord.gg/fVrRa8z')
+})
+
+app.get('*', (req, res) => {
+  res.status(404).render('404')
+})
+
+app.listen(port, () => {
+  console.log(`ASP Bots website is active and listening to port ${port}`)
+})
